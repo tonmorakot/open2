ecf9675a704cad852ef182e46c64714b48388bcd
Morakot Thongprom <tonmorakot@gmail.com>
2020-11-19 12:00:59 +0700

    1 test 1.0.0

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+// require __DIR__ . '/../vendor/autoload.php';
+
+// use Mike42\Escpos\PrintConnectors\FilePrintConnector;
+// use Mike42\Escpos\Printer;
+// $connector = new FilePrintConnector("php://stdout");
+// $printer = new Printer($connector);
+// $printer -> text("Hello World!\n");
+// $printer -> cut();
+// $printer -> close();
+
+echo "Open cash drawer";
